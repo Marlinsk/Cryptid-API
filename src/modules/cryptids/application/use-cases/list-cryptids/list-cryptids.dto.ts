bf4aacc6cac125ed684312de2d989ecb2cf2c106
Marlinsk <Marlonnlmx@outlook.com>
2025-12-27 00:10:24 -0300

    refactor: update either + dtos

--- src/modules/cryptids/application/use-cases/list-cryptids/list-cryptids.dto.ts
+++ src/modules/cryptids/application/use-cases/list-cryptids/list-cryptids.dto.ts
@@ -2,6 +2,12 @@ import { booleanFilterSchema, multivaluedNumberSchema, multivaluedStringSchema, 
 import { paginationQuerySchema } from '@shared/validators/pagination.validator'
 import { z } from 'zod'
 
+const fieldsParamSchema = z
+  .string()
+  .transform(val => val.split(',').map(v => v.trim()))
+  .pipe(z.array(z.string().min(1)))
+  .optional()
+
 export const listCryptidsSchema = z.object({
   search: z.string().min(1).optional(),
   habitat: z.union([z.coerce.number().int().positive(), multivaluedNumberSchema]).optional(),
@@ -14,6 +20,7 @@ export const listCryptidsSchema = z.object({
   limit: paginationQuerySchema.shape.limit,
   sort: z.enum(orderableFields).optional(),
   order: z.enum(['asc', 'desc']).default('asc'),
+  fields: fieldsParamSchema,
 })
 
 export type ListCryptidsDTO = z.infer<typeof listCryptidsSchema>
